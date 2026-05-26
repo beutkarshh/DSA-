46244e456f782df4d0854c6e4290ac73c50fd811
beutkarshh <utkarshwaghmare2004@gmail.com>
2026-05-26 11:50:17 +0530

    Conversion of array to lined list

---
+++ LinkedList/ConvertArr2LL.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+struct Node {
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1,Node* next1){
+        data=data1;
+        next=next1;
+
+    }
+};
+
+Node* CovertArr2LL(vector<int> &arr){
+    Node* head = new Node (arr[0],NULL);
+    Node* mover = head;
+    for (int i=1;i<arr.size();i++){
+        Node* temp = new Node (arr[i],NULL);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+int main(){
+    vector<int>arr={2,5,8,7};
+    Node* head = CovertArr2LL(arr);
+    cout<<head->data<<endl;
+    
+}
